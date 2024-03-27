f4d8548fcf669be7383fa59d8290ad5bd8281ba2
Daniel Adámek <daniel.adamek123@icloud.com>
2024-03-27 21:41:56 +0100

    Payment model
    Signed-off-by: Daniel Adámek <daniel.adamek123@icloud.com>

--- src/Model/Payment.php
+++ src/Model/Payment.php
@@ -6,6 +6,13 @@ use Datetime;
 use StinWeatherApp\Model\Types\Currency;
 use StinWeatherApp\Model\Types\PaymentType;
 
+/**
+ * Class Payment
+ *
+ * @author Daniel Adámek
+ * @description Model for payment
+ * @package StinWeatherApp\Model
+ */
 class Payment {
 	private float $amount;
 	private Currency $currency;
@@ -13,11 +20,120 @@ class Payment {
 	private PaymentType $type;
 	private string $status;
 
+	/**
+	 * Payment constructor.
+	 *
+	 * @param float       $amount
+	 * @param Currency    $currency
+	 * @param DateTime    $datetime
+	 * @param PaymentType $type
+	 * @param string      $status
+	 */
 	public function __construct(float $amount, Currency $currency, DateTime $datetime, PaymentType $type, string $status) {
 		$this->amount = $amount;
 		$this->currency = $currency;
 		$this->datetime = $datetime;
 		$this->type = $type;
 		$this->status = $status;
+	}
+
+	/**
+	 * @description Sets the amount
+	 *
+	 * @param float $amount
+	 *
+	 * @return Payment
+	 */
+	public function setAmount(float $amount): Payment {
+		$this->amount = $amount;
+		return $this;
+	}
+
+	/**
+	 * @description Gets the amount
+	 * @return float
+	 */
+	public function getAmount(): float {
+		return $this->amount;
+	}
+
+	/**
+	 * @description Sets the currency
+	 *
+	 * @param Currency $currency
+	 *
+	 * @return Payment
+	 */
+	public function setCurrency(Currency $currency): Payment {
+		$this->currency = $currency;
+		return $this;
+	}
+
+	/**
+	 * @description Gets the currency
+	 * @return Currency
+	 */
+	public function getCurrency(): Currency {
+		return $this->currency;
+	}
+
+	/**
+	 * @description Sets the datetime
+	 *
+	 * @param DateTime $datetime
+	 *
+	 * @return Payment
+	 */
+	public function setDatetime(Datetime $datetime): Payment {
+		$this->datetime = $datetime;
+		return $this;
+	}
+
+	/**
+	 * @description Gets the datetime
+	 * @return DateTime
+	 */
+	public function getDatetime(): Datetime {
+		return $this->datetime;
+	}
+
+	/**
+	 * @description Sets the type
+	 *
+	 * @param PaymentType $type
+	 *
+	 * @return Payment
+	 */
+	public function setType(PaymentType $type): Payment {
+		$this->type = $type;
+		return $this;
+	}
+
+	/**
+	 * @description Gets the type
+	 * @return PaymentType
+	 */
+	public function getType(): PaymentType {
+		return $this->type;
+	}
+
+	/**
+	 * @description Sets the status
+	 *
+	 * @param string $status
+	 *
+	 * @return Payment
+	 */
+	public function setStatus(string $status): Payment {
+		$this->status = $status;
+		return $this;
+	}
+
+	/**
+	 * @description Gets the status
+	 * @return string
+	 */
+	public function getStatus(): string {
+		return $this->status;
 	}
 }

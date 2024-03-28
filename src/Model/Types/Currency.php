f0b8c32b1eb94269b89fc7b1db0c7a2281184ebf
Daniel Adámek <daniel.adamek123@icloud.com>
2024-03-28 10:22:09 +0100

    currency is valid
    Signed-off-by: Daniel Adámek <daniel.adamek123@icloud.com>

--- src/Model/Types/Currency.php
+++ src/Model/Types/Currency.php
@@ -5,4 +5,14 @@ namespace StinWeatherApp\Model\Types;
 enum Currency: string {
 	case CZK = "CZK";
 	case EUR = "EUR";
+
+	public static function isValid(string $value): bool {
+		foreach (self::cases() as $case) {
+			if ($value === $case->value) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 }

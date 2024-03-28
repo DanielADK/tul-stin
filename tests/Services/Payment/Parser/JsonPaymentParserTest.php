9a1f6586ea2fe93a2508e0e3c3a47b45af29eed2
Daniel Adámek <daniel.adamek123@icloud.com>
2024-03-28 09:33:24 +0100

    Json parser test
    Signed-off-by: Daniel Adámek <daniel.adamek123@icloud.com>

--- tests/Services/Payment/Parser/JsonPaymentParserTest.php
+++ tests/Services/Payment/Parser/JsonPaymentParserTest.php
@@ -9,6 +9,7 @@ use StinWeatherApp\Model\Payment;
 use StinWeatherApp\Model\Types\Currency;
 use StinWeatherApp\Model\Types\PaymentType;
 use StinWeatherApp\Services\Payment\Parser\JsonPaymentParser;
+use ValueError;
 
 class JsonPaymentParserTest extends TestCase {
 
@@ -96,6 +97,128 @@ class JsonPaymentParserTest extends TestCase {
 			'amount' => -100.0, // Invalid value: amount cannot be negative
 			'currency' => 'EUR',
 			'type' => 'CASH'
+		]);
+		$this->assertTrue(is_string($data));
+
+		$this->parser->parse($data);
+	}
+
+	/**
+	 * Test parsing with unknown keys.
+	 *
+	 * @throws Exception
+	 */
+	public function testParseWithUnknownKeys(): void {
+		$data = json_encode([
+			'amount' => 100.0,
+			'currency' => 'EUR',
+			'type' => 'CARD',
+			'unknownKey' => 'unknownValue'
+		]);
+		$this->assertTrue(is_string($data));
+
+		$payment = $this->parser->parse($data);
+
+		$this->assertInstanceOf(Payment::class, $payment);
+	}
+
+	/**
+	 * Test parsing with empty strings.
+	 *
+	 * @throws Exception
+	 */
+	public function testParseWithEmptyStrings(): void {
+		$this->expectException(\TypeError::class);
+
+		$data = json_encode([
+			'amount' => '',
+			'currency' => '',
+			'type' => ''
+		]);
+		$this->assertTrue(is_string($data));
+
+		$this->parser->parse($data);
+	}
+
+	/**
+	 * Test parsing with null values.
+	 *
+	 * @throws Exception
+	 */
+	public function testParseWithNullValues(): void {
+		$this->expectException(\TypeError::class);
+
+		$data = json_encode([
+			'amount' => null,
+			'currency' => null,
+			'type' => null
+		]);
+		$this->assertTrue(is_string($data));
+
+		$this->parser->parse($data);
+	}
+
+	/**
+	 * Test parsing with invalid data types.
+	 *
+	 * @throws Exception
+	 */
+	public function testParseWithInvalidDataTypes(): void {
+		$this->expectException(ValueError::class);
+
+		$data = json_encode([
+			'amount' => '100',
+			'currency' => 100,
+			'type' => 100
+		]);
+		$this->assertTrue(is_string($data));
+
+		$this->parser->parse($data);
+	}
+
+	/**
+	 * Test parsing with various date and time formats.
+	 *
+	 * @throws Exception
+	 */
+	public function testParseWithVariousDatetimeFormats(): void {
+		$data = json_encode([
+			'amount' => 100.0,
+			'currency' => 'EUR',
+			'type' => 'CASH',
+			'datetime' => '01/01/2022'
+		]);
+		$this->assertTrue(is_string($data));
+
+		$payment = $this->parser->parse($data);
+
+		$this->assertInstanceOf(Payment::class, $payment);
+
+		$data = json_encode([
+			'amount' => 100.0,
+			'currency' => 'EUR',
+			'type' => 'CARD',
+			'datetime' => '2022-01-01T00:00:00Z'
+		]);
+		$this->assertTrue(is_string($data));
+
+		$payment = $this->parser->parse($data);
+
+		$this->assertInstanceOf(Payment::class, $payment);
+	}
+
+	/**
+	 * Test parsing with invalid currency and payment type values.
+	 *
+	 * @throws Exception
+	 */
+	public function testParseWithInvalidCurrencyAndPaymentTypeValues(): void {
+		$this->expectException(ValueError::class);
+
+		$data = json_encode([
+			'amount' => 100.0,
+			'currency' => 'INVALID_CURRENCY',
+			'type' => 'INVALID_TYPE'
 		]);
 		$this->assertTrue(is_string($data));
 

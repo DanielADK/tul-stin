6ac6fa13ebd347ca70e2bf54653f26657778edd1
Daniel Adámek <daniel.adamek123@icloud.com>
2024-03-31 23:16:46 +0200

    XMLPayment + tests
    Signed-off-by: Daniel Adámek <daniel.adamek123@icloud.com>

---
+++ tests/Services/Payment/Parser/XMLPaymentParserTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Services\Payment\Parser;
+
+use Exception;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use StinWeatherApp\Services\Payment\Parser\XMLPaymentParser;
+
+class XMLPaymentParserTest extends TestCase {
+
+	private XMLPaymentParser $parser;
+
+	/**
+	 * Test that the parse method correctly processes valid XML data and returns the expected Payment object.
+	 *
+	 * @throws Exception
+	 */
+	public function testParseValidXML(): void {
+		$xml = '<payment><amount>100</amount><currency>CZK</currency><type>CASH</type><datetime>2022-01-01T00:00:00</datetime><status>pending</status></payment>';
+
+		$payment = $this->parser->parse($xml);
+
+		$this->assertSame(100.0, $payment->getAmount());
+		$this->assertSame('CZK', $payment->getCurrency()->value);
+		$this->assertSame('CASH', $payment->getType()->value);
+		$this->assertSame('2022-01-01T00:00:00', $payment->getDatetime()->format('Y-m-d\TH:i:s'));
+		$this->assertSame('pending', $payment->getStatus());
+	}
+
+	/**
+	 * Test that the parse method throws an InvalidArgumentException if invalid XML data is provided.
+	 *
+	 * @throws Exception
+	 */
+	public function testParseInvalidXML(): void {
+		$this->expectException(InvalidArgumentException::class);
+		$this->expectExceptionMessage('Invalid XML provided');
+
+		$xml = 'invalid xml';
+		$this->parser->parse($xml);
+	}
+
+	/**
+	 * Test that the parse method throws an InvalidArgumentException if any of the required keys are missing in the XML
+	 * data.
+	 *
+	 * @throws Exception
+	 */
+	public function testParseMissingRequiredKeys(): void {
+		$this->expectException(InvalidArgumentException::class);
+		$this->expectExceptionMessage('Missing required key: amount');
+
+		$xml = '<payment><currency>CZK</currency><type>CASH</type><datetime>2022-01-01T00:00:00</datetime><status>pending</status></payment>';
+		$this->parser->parse($xml);
+	}
+
+	/**
+	 * Test that the parse method throws an InvalidArgumentException if an invalid currency value is provided in the
+	 * XML data.
+	 *
+	 * @throws Exception
+	 */
+	public function testParseInvalidCurrency(): void {
+		$this->expectException(InvalidArgumentException::class);
+		$this->expectExceptionMessage('Invalid currency value: INVALID');
+
+		$xml = '<payment><amount>100</amount><currency>INVALID</currency><type>CASH</type><datetime>2022-01-01T00:00:00</datetime><status>pending</status></payment>';
+		$this->parser->parse($xml);
+	}
+
+	protected function setUp(): void {
+		$this->parser = new XMLPaymentParser();
+	}
+}

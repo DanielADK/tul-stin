addf1bf5b3dd077801ca64b9d6f162ad89d5768f
Daniel Adámek <daniel.adamek123@icloud.com>
2024-04-01 21:07:27 +0200

    path for pay
    Signed-off-by: Daniel Adámek <daniel.adamek123@icloud.com>

--- index.php
+++ index.php
@@ -7,9 +7,10 @@ use StinWeatherApp\Component\Database\SQLiteConnectionBuilder;
 use StinWeatherApp\Component\Http\Method;
 use StinWeatherApp\Component\Router\Route;
 use StinWeatherApp\Component\Router\Router;
+use StinWeatherApp\Controller\Api\PaymentController as ApiPaymentController;
 use StinWeatherApp\Controller\HomeController;
 use StinWeatherApp\Controller\NotFoundController;
-use StinWeatherApp\Controller\PaymentController;
+use StinWeatherApp\Controller\PayController;
 
 // Init router
 $router = new Router();
@@ -27,7 +28,10 @@ try {
 // Routes
 $router->addRoute("/", HomeController::class);
 $router->setNotFound(new Route("/not-found", NotFoundController::class, "index"));
-$router->addRoute("/pay", PaymentController::class, "paymentProcessing", Method::POST);
+$router->addRoute("/pay", PayController::class, "paymentForm", Method::GET);
 $router->addRoute("/time", HomeController::class, "time");
+
+// API Routes
+$router->addRoute("/api/payment", ApiPaymentController::class, "processPayment", Method::POST);
 
 $router->dispatch($_SERVER["REQUEST_URI"], Method::from($_SERVER["REQUEST_METHOD"]));

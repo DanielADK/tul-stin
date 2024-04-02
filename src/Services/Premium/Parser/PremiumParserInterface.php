0a0ae3a916292d819d0ebce21040fe9a382d04ff
Daniel Adámek <daniel.adamek123@icloud.com>
2024-04-02 17:17:13 +0200

    premium parse interface
    Signed-off-by: Daniel Adámek <daniel.adamek123@icloud.com>

--- src/Services/Premium/Parser/PremiumParserInterface.php
+++ src/Services/Premium/Parser/PremiumParserInterface.php
@@ -7,5 +7,12 @@ use StinWeatherApp\Model\Buyable\Premium;
 interface PremiumParserInterface {
 	const array requiredKeys = ['username', 'email', 'premiumOption'];
 
+	/**
+	 * @description Parses the data and returns a Premium object
+	 *
+	 * @param string $data
+	 *
+	 * @return Premium
+	 */
 	public function parse(string $data): Premium;
 }

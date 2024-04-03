d53dd9ce5f361bbcb2a3b5fb9793bd8633d8e179
Daniel Adámek <daniel.adamek123@icloud.com>
2024-04-03 23:05:49 +0200

    premium list from db
    Signed-off-by: Daniel Adámek <daniel.adamek123@icloud.com>

--- src/Controller/PayController.php
+++ src/Controller/PayController.php
@@ -3,10 +3,15 @@
 namespace StinWeatherApp\Controller;
 
 use StinWeatherApp\Component\Http\Response;
+use StinWeatherApp\Model\Buyable\Premium;
 
 class PayController extends AbstractController {
 
 	public function paymentForm(): Response {
-		return $this->render("pay");
+		$premiumList = Premium::getListOfPremiums();
+
+		return $this->render("pay", [
+			"premiumList" => $premiumList
+		]);
 	}
 }

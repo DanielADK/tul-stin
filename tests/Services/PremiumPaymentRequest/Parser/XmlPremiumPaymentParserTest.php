3aef6ac66675ca2faa8645f005369aefbba7aacf
Daniel Adámek <daniel.adamek123@icloud.com>
2024-04-03 17:50:38 +0200

    assertions without warnings
    Signed-off-by: Daniel Adámek <daniel.adamek123@icloud.com>

--- tests/Services/PremiumPaymentRequest/Parser/XmlPremiumPaymentParserTest.php
+++ tests/Services/PremiumPaymentRequest/Parser/XmlPremiumPaymentParserTest.php
@@ -4,12 +4,16 @@ namespace Services\PremiumPaymentRequest\Parser;
 
 use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
-use StinWeatherApp\Services\Payment\Parser\XmlPaymentParser;
 use StinWeatherApp\Services\PremiumPaymentRequest\Parser\XmlPremiumPaymentParser;
 
 class XmlPremiumPaymentParserTest extends TestCase {
 
 	private XmlPremiumPaymentParser $parser;
+
+	protected function setUp(): void {
+		$this->parser = new XmlPremiumPaymentParser();
+		error_reporting(E_ALL);
+	}
 
 	public function testParseWithValidInput(): void {
 		$xml = '<root><username>test</username><premiumOption>option1</premiumOption><paymentType>card</paymentType><card><cardNumber>1234567890123456</cardNumber><cardExpiration>12/23</cardExpiration><cardCode>123</cardCode></card></root>';
@@ -38,12 +42,6 @@ class XmlPremiumPaymentParserTest extends TestCase {
 		$this->expectException(InvalidArgumentException::class);
 
 		$xml = 'not valid xml';
-		$this->parser->parse($xml);
+		@$this->parser->parse($xml);
 	}
-
-	protected function setUp(): void {
-		$this->parser = new XmlPremiumPaymentParser();
-		error_reporting(E_ALL);
-	}
-
 }

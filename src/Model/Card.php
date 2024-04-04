3d4f4a317841e083154347179be5d5ab02759a80
Daniel Adámek <daniel.adamek123@icloud.com>
2024-04-04 12:05:47 +0200

    Card validation
    Signed-off-by: Daniel Adámek <daniel.adamek123@icloud.com>

--- src/Model/Card.php
+++ src/Model/Card.php
@@ -24,9 +24,9 @@ class Card {
 	 * @param string $code
 	 */
 	public function __construct(string $number, string $expiration, string $code) {
-		$this->number = $number;
-		$this->expiration = $expiration;
-		$this->code = $code;
+		$this->setNumber($number);
+		$this->setExpiration($expiration);
+		$this->setCode($code);
 	}
 
 	public function getNumber(): string {
@@ -34,6 +34,9 @@ class Card {
 	}
 
 	public function setNumber(string $number): Card {
+		if (!$this->validateNumber($number)) {
+			throw new \InvalidArgumentException('Invalid card number. Must be 16 digits.');
+		}
 		$this->number = $number;
 		return $this;
 	}
@@ -43,6 +46,9 @@ class Card {
 	}
 
 	public function setExpiration(string $expiration): Card {
+		if (!$this->validateExpiration($expiration)) {
+			throw new \InvalidArgumentException('Invalid expiration date. Must be in format MM/YY.');
+		}
 		$this->expiration = $expiration;
 		return $this;
 	}
@@ -52,8 +58,44 @@ class Card {
 	}
 
 	public function setCode(string $code): Card {
+		if (!$this->validateCode($code)) {
+			throw new \InvalidArgumentException('Invalid code. Must be 3 digits.');
+		}
 		$this->code = $code;
 		return $this;
+	}
+
+	/**
+	 * @description Validates the card number
+	 *
+	 * @param string $number
+	 *
+	 * @return bool
+	 */
+	public function validateNumber(string $number): bool {
+		return (preg_match('/^\d{16}$/', $number) !== false);
+	}
+
+	/**
+	 * @description Validates the expiration
+	 *
+	 * @param string $expiration
+	 *
+	 * @return bool
+	 */
+	public function validateExpiration(string $expiration): bool {
+		return (preg_match('/^\d{2}\/\d{2}$/', $expiration) !== false);
+	}
+
+	/**
+	 * @description Validates the code
+	 *
+	 * @param string $code
+	 *
+	 * @return bool
+	 */
+	public function validateCode(string $code): bool {
+		return (preg_match('/^\d{3}$/', $code) !== false);
 	}
 
 }

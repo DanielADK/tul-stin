cdf49c2511dff35d8199f804c35debb7415dfefe
Daniel Adámek <daniel.adamek123@icloud.com>
2024-04-06 20:09:44 +0200

    xml mute php warnings
    Signed-off-by: Daniel Adámek <daniel.adamek123@icloud.com>

--- tests/Component/Parser/XmlParseableTest.php
+++ tests/Component/Parser/XmlParseableTest.php
@@ -8,6 +8,13 @@ use StinWeatherApp\Component\Parser\XmlParseable;
 class XmlParseableTest extends TestCase {
 	private XmlParseable $xmlParseable;
 
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->xmlParseable = new XmlParseable();
+		error_reporting(E_ALL);
+	}
+
 	/**
 	 * Test if the class can be instantiated.
 	 */
@@ -22,12 +29,6 @@ class XmlParseableTest extends TestCase {
 		$validXml = '<root><key>value</key></root>';
 		$invalidXml = '<root><key>value</key>';
 		$this->assertTrue($this->xmlParseable->canParse($validXml));
-		$this->assertFalse($this->xmlParseable->canParse($invalidXml));
-	}
-
-	protected function setUp(): void {
-		parent::setUp();
-
-		$this->xmlParseable = new XmlParseable();
+		@$this->assertFalse($this->xmlParseable->canParse($invalidXml));
 	}
 }
